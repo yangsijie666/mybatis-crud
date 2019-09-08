78dccf08c373ac1485da987580afab3522255066
yangsijie <yangsijie151104@126.com>
2019-09-08 20:48:39 +0800

    配置文件的解析与优化

--- src/main/java/cn/sxt/util/MyBatisUtil.java
+++ src/main/java/cn/sxt/util/MyBatisUtil.java
@@ -9,12 +9,22 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class MyBatisUtil {
+    /**
+     * 通过文件创建SqlSessionFactory，是SqlSession的工厂类
+     * @return
+     * @throws IOException
+     */
     public static SqlSessionFactory getSqlSessionFactory() throws IOException {
         String resource = "mybatis.cfg.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         return new SqlSessionFactoryBuilder().build(inputStream);
     }
 
+    /**
+     * SqlSession通过id找到对应的SQL语句，执行SQL语句
+     * @return
+     * @throws IOException
+     */
     public static SqlSession getSession() throws IOException {
         return getSqlSessionFactory().openSession();
     }
